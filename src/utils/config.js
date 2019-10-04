65d5d39ed3d6b46f97843932686814d37fd3009c
MRoyM <michael_mather@hotmail.com>
2019-10-04 16:22:38 -0230

    Update config for deployment

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,10 +3,8 @@ const getBaseUrl = () => {
     switch (env) {
         case 'local':
             return 'http://127.0.0.1:3000/';
-        case 'development':
-            return 'http://127.0.0.1:3000/';
         case 'staging':
-            return 'http://127.0.0.1:3000/';
+            return 'https://staging-api.bivver.ca/';
         default:
             return 'http://127.0.0.1:3000/';
     }
@@ -21,7 +19,7 @@ const config = {
     STRIPE_CLIENT_ID: process.env.NODE_ENV === 'production' ? 'ca_Ehf6IL9J9llQ9DmC4eM04M1PxLa7Sw2P' : 'ca_Ehf6RukQblf1Rg23P0cHAdmBYs7cDGny',
     STRIPE_DASHBOARD_URL: 'https://dashboard.stripe.com/dashboard',
     RECAPTCHA_SITE_KEY: '6LdU7qYUAAAAAGQbwfzdgNrrmxKJp-eNA-MM4jIO',
-    S3_IMAGE_BUCKET: 'https://s3.ca-central-1.amazonaws.com/vinocount-images/'
+    S3_IMAGE_BUCKET: 'https://s3.ca-central-1.amazonaws.com/bivver-public/'
 };
 
 export default config;
