d19375392304a339a02abbfd020043892e047093
MRoyM <michael_mather@hotmail.com>
2019-10-08 11:30:22 -0230

    Fix environment not being set correctly

--- src/utils/config.js
+++ src/utils/config.js
@@ -3,8 +3,8 @@ const config = {
     BASE_URL: process.env.REACT_APP_DOMAIN,
     ENV: 'local',
     TABLET_BREAK: 960,
-    STRIPE_API_KEY: process.env.NODE_ENV === 'production' ? 'pk_live_kDlSeQ83oNFpI4m6zE1gYWMC' : 'pk_test_BJHlY3dKiWDpjGVihty1Zwm9',
-    STRIPE_CLIENT_ID: process.env.NODE_ENV === 'production' ? 'ca_Ehf6IL9J9llQ9DmC4eM04M1PxLa7Sw2P' : 'ca_Ehf6RukQblf1Rg23P0cHAdmBYs7cDGny',
+    STRIPE_API_KEY: process.env.REACT_NODE_ENV === 'production' ? 'pk_live_kDlSeQ83oNFpI4m6zE1gYWMC' : 'pk_test_BJHlY3dKiWDpjGVihty1Zwm9',
+    STRIPE_CLIENT_ID: process.env.REACT_NODE_ENV === 'production' ? 'ca_Ehf6IL9J9llQ9DmC4eM04M1PxLa7Sw2P' : 'ca_Ehf6RukQblf1Rg23P0cHAdmBYs7cDGny',
     STRIPE_DASHBOARD_URL: 'https://dashboard.stripe.com/dashboard',
     RECAPTCHA_SITE_KEY: '6LfZVbwUAAAAANirNfe6AlRtsOHDRW4mSkmIl3aR',
     S3_IMAGE_BUCKET: 'https://bivver-public.s3.amazonaws.com/'
