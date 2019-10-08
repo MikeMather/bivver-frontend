ea3afcdda2a02adc6c7a57350cca6d0f3efce842
MRoyM <michael_mather@hotmail.com>
2019-10-08 12:32:36 -0230

    Fix recaptcha

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,7 +6,7 @@ const config = {
     STRIPE_API_KEY: process.env.REACT_NODE_ENV === 'production' ? 'pk_live_kDlSeQ83oNFpI4m6zE1gYWMC' : 'pk_test_BJHlY3dKiWDpjGVihty1Zwm9',
     STRIPE_CLIENT_ID: process.env.REACT_NODE_ENV === 'production' ? 'ca_Ehf6IL9J9llQ9DmC4eM04M1PxLa7Sw2P' : 'ca_Ehf6RukQblf1Rg23P0cHAdmBYs7cDGny',
     STRIPE_DASHBOARD_URL: 'https://dashboard.stripe.com/dashboard',
-    RECAPTCHA_SITE_KEY: '6Ley_bsUAAAAADR4CwVlp2UH6LcLjB5mX7eHFJwm',
+    RECAPTCHA_SITE_KEY: '6LfZVbwUAAAAANirNfe6AlRtsOHDRW4mSkmIl3aR',
     S3_IMAGE_BUCKET: 'https://bivver-public.s3.amazonaws.com/'
 };
 
